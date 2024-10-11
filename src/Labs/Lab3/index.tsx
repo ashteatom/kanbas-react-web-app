4faae4c0dd3f483fcd8d125472c488a19af4fa23
Ains <ashthekettle@gmail.com>
2024-10-11 14:48:39 -0400

    Completed A3 3.2 -- forgot to switch branches

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -36,6 +36,7 @@ import AddPathParameters from "./AddPathParameters";
 
 
 export default function Lab3() {
+    console.log('Hello World!');
     return (
       <div id = "wd-lab3">
         <h2>Lab 3</h2>
