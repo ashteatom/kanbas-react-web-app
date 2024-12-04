6ad647ab00b59ad6f870005455983c83fbd86c62
Ains <ashthekettle@gmail.com>
2024-12-04 00:45:03 -0500

    commit to merg

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -38,11 +38,6 @@ export default function Assignments() {
     }
   };
   
-  /* const fetchAssignments = async () => {
-    const assignments = await coursesClient.findAssignmentsForCourse(cid as string);
-    dispatch(setAssignments(assignments));
-  }; */
-  
   useEffect(() => {
     fetchAssignments();
   }, []);
