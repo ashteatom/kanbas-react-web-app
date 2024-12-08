82eff56e73ba8e2a37ddc731c85351bb1f79e6fd
Ains <ashthekettle@gmail.com>
2024-12-07 23:22:15 -0500

    4.1.2

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -38,6 +38,48 @@ export const createCourse = async (course: any) => {
   return data;
 };
 
+export const findAllUsers = async () => {
+  const response = await axiosWithCredentials.get(USERS_API);
+  return response.data;
+};
+
+export const findUsersByRole = async (role: string) => {
+  const response = await
+    axios.get(`${USERS_API}?role=${role}`);
+  return response.data;
+};
+
+export const findUsersByPartialName = async (name: string) => {
+  const response = await axios.get(`${USERS_API}?name=${name}`);
+  return response.data;
+};
+
+export const findUserById = async (id: string) => {
+  const response = await axios.get(`${USERS_API}/${id}`);
+  return response.data;
+};
+
+export const deleteUser = async (userId: string) => {
+  const response = await axios.delete( `${USERS_API}/${userId}` );
+  return response.data;
+};
+
+export const updateEmail = async (user: any, email: string) => {
+  const response = await axiosWithCredentials.put(`${USERS_API}/${user._id}`, email);
+  return response.data;
+};
   
+export const updateRole = async (user: any, role: string) => {
+  const response = await axiosWithCredentials.put(`${USERS_API}/${user._id}`, role);
+  return response.data;
+};
+
+export const createUser = async (user: any) => {
+  const response = await axios.post(`${USERS_API}`, user);
+  return response.data;
+};
+
+
+
 
   
